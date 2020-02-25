c4a7ab2395041720dca237d7d26c5f3d26a91a52
Unknown <maxscaddie@gmail.com>
2020-02-26 10:14:37 +1100

    US4.4 Added if name == main

--- input_system/ballot_writer.py
+++ input_system/ballot_writer.py
@@ -1,6 +1,7 @@
 import csv
 
-mock_write_data = [['S', 'M', 'L', 'O'], ['M', 'L', 'O'], ['M', 'S', 'O', 'L'], ['O', 'S']]
+mock_write_data = [['S', 'M', 'L', 'O'], [
+    'M', 'L', 'O'], ['M', 'S', 'O', 'L'], ['O', 'S']]
 
 
 def write_raw_ballots_to_csv(raw_ballots, csv_filename):
@@ -10,4 +11,8 @@ def write_raw_ballots_to_csv(raw_ballots, csv_filename):
         writer.writerow(row)
     FILE.close()
 
+
 write_raw_ballots_to_csv(mock_write_data, "raw_data_csv.csv")
+
+if __name__ == "__main__":
+    print()
--- input_system/main.py
+++ input_system/main.py
@@ -4,12 +4,14 @@ from read_in_csv import *
 from ballot_processor import *
 from ballot_writer import *
 
-excel_path = input("Please enter ballot data excel filename exactly: \n")
 
-if (excel_path[-5:] != '.xlsx'):
-    excel_path = excel_path + ".xlsx"
+if __name__ == "__main__":
+    excel_path = input("Please enter ballot data excel filename exactly: \n")
 
-input_ballots = read_in_votes(excel_path)
-styles = read_in_styles(excel_path)
-output_ballots = standardise_ballot_styles(input_ballots, styles)
-write_raw_ballots_to_csv(output_ballots,"raw_ballots.csv")
+    if (excel_path[-5:] != '.xlsx'):
+        excel_path = excel_path + ".xlsx"
+
+    input_ballots = read_in_votes(excel_path)
+    styles = read_in_styles(excel_path)
+    output_ballots = standardise_ballot_styles(input_ballots, styles)
+    write_raw_ballots_to_csv(output_ballots, "raw_ballots.csv")
