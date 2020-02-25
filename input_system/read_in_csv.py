250dd6ac9e666c3ca0ada03f85048a5912035510
Unknown <maxscaddie@gmail.com>
2020-02-25 14:14:41 +1100

    US1.2 Refactor and made callable

--- input_system/read_in_csv.py
+++ input_system/read_in_csv.py
@@ -1,27 +1,11 @@
 import openpyxl
 
-
-def read_in_excel(excel_path):
-    vote_excel_spreadsheet = openpyxl.load_workbook(excel_path)
-    sheet_names = vote_excel_spreadsheet.sheetnames
-
-    vote_sheet_name = sheet_names[0]
-    style_sheet_name = sheet_names[1]
-
-    votes = read_in_votes(vote_excel_spreadsheet, vote_sheet_name)
-    styles = read_in_styles(vote_excel_spreadsheet, style_sheet_name)
-
-    print("\nVotes")
-    print(votes)
-    print("\nStyles")
-    print(styles)
+TEST_DATA_PATH = "./test_data.xlsx"
 
 
-def read_in_votes(vote_excel_spreadsheet, vote_sheet_name):
-    vote_sheet = vote_excel_spreadsheet[vote_sheet_name]
-
+def read_in_votes(excel_path):
+    vote_sheet = get_sheet_by_number(excel_path, 0)
     columns_with_ballots = list(vote_sheet.columns)[1:]
-
     array_of_ballots = []
 
     for column in columns_with_ballots:
@@ -32,12 +16,11 @@ def read_in_votes(vote_excel_spreadsheet, vote_sheet_name):
     return array_of_ballots
 
 
-def read_in_styles(vote_excel_spreadsheet, style_sheet_name):
-    style_sheet = vote_excel_spreadsheet[style_sheet_name]
-
+def read_in_styles(excel_path):
+    style_sheet = get_sheet_by_number(excel_path, 1)
     rows_with_styles = list(style_sheet.rows)[1:]
-
     styles = []
+
     for row in rows_with_styles:
         style = []
         for cell in row:
@@ -46,4 +29,9 @@ def read_in_styles(vote_excel_spreadsheet, style_sheet_name):
     return styles
 
 
-read_in_excel("./test_data.xlsx")
+def get_sheet_by_number(excel_path, number):
+    vote_excel_spreadsheet = openpyxl.load_workbook(excel_path)
+    sheet_names = vote_excel_spreadsheet.sheetnames
+    sheet_name = sheet_names[number]
+
+    return vote_excel_spreadsheet[sheet_name]
