641b9e1e0be0aebad9675c295d4d4654b6c47a54
Unknown <maxscaddie@gmail.com>
2020-02-27 09:59:04 +1100

    US6.2 Prepare the files for the new US

--- counting_system/candidate_finder.py
+++ counting_system/candidate_finder.py
@@ -1,3 +1,34 @@
+# US9
+def calculate_candidate_points(ballots):
+    list_of_candidates_and_their_points = []
+    # US9.1: build a points list [(point_value, "name")] for every candidate
+
+    # US9.2: function to parse ballots to accumulate points for each 1st pref candidate and update points list
+    return list_of_candidates_and_their_points
+
 # US10
+
+
+def elect_a_candidate(candidate_point_list, quota):
+    highest_candidate_tuple = None
+    # US10.1: sort candidate point list
+
+    # US10.2: if highest person achieves quota, return their name, otherwise, return None
+
+    return highest_candidate_tuple
+
 # US13
+
+
+def find_lowest_point_candidate(candidate_point_list):
+    lowest_points_candidate_name = ""
+
+    return lowest_points_candidate_name
+
 # US16
+
+
+def calculate_exhausted_points(candidate_name, ballots):
+    number_of_points_with_only_candidate = 0
+
+    return number_of_points_with_only_candidate
--- counting_system/data_loader.py
+++ counting_system/data_loader.py
@@ -1,2 +1,11 @@
 # US7
-# US14
+def load_in_data():
+    ballots = []
+    # US7.1 read in CSV of ballots
+
+    # US7.2 Read in by input # vacancices
+
+    # US7.3 Make 1000 first element of every ballot
+
+    return ballots
+# US14 Stats
