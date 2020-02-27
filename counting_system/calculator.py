9fb61b1729c0b56081169f8a53f724c047637ea7
Luke Jacobson <jukelacobson@gmail.com>
2020-02-27 23:58:31 +1100

    US8,11,12,15,16: first draft of finished functions

--- counting_system/calculator.py
+++ counting_system/calculator.py
@@ -1,35 +1,20 @@
-# US8 - calculate quota
-# US11 - calculate TF
-
-# confirm below US16 with max
-# US16 - calculate exhuausted points
 POINTS_PER_BALLOT = 1000
 
+# US8 - calculate quota
 def calculate_quota(ballot_list, vacancies):
     formal_votes = len(ballot_list)
     return int(formal_votes*POINTS_PER_BALLOT/(vacancies + 1) + 1)
 
-# takes in: candidate name, points array, quota
-# spits out: transfer factor
+# US11 - calculate TF
+def calculate_tranfer_factor(ballot_list, candidate_tuple, quota):
+    achieved_points = candidate_tuple[0]
+    candidate_exhuausted_points = calculate_exhausted_points(candidate_tuple, ballot_list)
+    return (achieved_points - quota)/(achieved_points - candidate_exhuausted_points)    
 
-def calculate_exhausted_points(candidate, ballot_list):
+# US16 - calculate exhuausted points
+def calculate_exhausted_points(candidate_tuple, ballot_list):
     exhuausted_points = 0
     for ballot in ballot_list:
-        if len(ballot) == 2 and ballot[1] == candidate:
+        if len(ballot) == 2 and ballot[1] == candidate_tuple[1]:
             exhuausted_points += ballot[0]
-    
     return exhuausted_points
-
-
-
-def calculate_tranfer_factor(ballot_list, candidate_name, candidate_points, quota):
-    achieved_points = 0
-    candidate_exhuausted_points = calculate_exhausted_points(candidate_name, ballot_list)
-
-    # If we can change output of US10 and pass a tuple (pts,'name') in place of candidate_name
-    # then this loop can be removed :)
-    for (points,name) in candidate_points:
-        if name == candidate_name:
-            achieved_points = points
-
-    return (achieved_points - quota)/(achieved_points - candidate_exhuausted_points)    
