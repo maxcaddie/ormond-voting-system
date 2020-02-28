1c6221b091078b37b48c53f6ba2696bfb6584a4b
Unknown <maxscaddie@gmail.com>
2020-02-28 11:01:10 +1100

    US7.2 Read in by input # vacancies

--- counting_system/data_loader.py
+++ counting_system/data_loader.py
@@ -7,19 +7,28 @@ def clean_row_turn_into_array(row):
     return array_row
 
 
-def load_in_data(csv_of_ballots_path):
+def load_in_ballot_data(csv_of_ballots_path):
     ballots = []
-    # US7.1 read in CSV of ballots
+    # US7.1 read in CSV of ballots US7.3 add 1000 to start
     ballot_csv = open(csv_of_ballots_path, 'r')
     for row in ballot_csv:
         a_ballot = clean_row_turn_into_array(row)
-
-    # US7.2 Read in by input # vacancices
-
-    # US7.3 Make 1000 first element of every ballot
-
+        a_ballot.insert(0, 1000)
+        ballots.append(a_ballot)
     return ballots
+
+
+def load_in_number_of_vacancies():
+    while True:
+        try:
+            vacancies = int(input("Enter the number of vacancies: "))
+        except ValueError:
+            print("Not an integer please re-enter a number")
+            continue
+        else:
+            return vacancies
+
 # US14 Stats
 
 
-print(load_in_data("./out4.csv"))
+print(load_in_number_of_vacancies())
