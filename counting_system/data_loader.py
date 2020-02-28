ca8f03a872a952c0a6791da6d2f05f444544c389
jukelacobson1 <47473164+jukelacobson1@users.noreply.github.com>
2020-02-28 13:03:56 +1100

    some more small changes

--- counting_system/data_loader.py
+++ counting_system/data_loader.py
@@ -1,6 +1,3 @@
-# US7
-
-
 def clean_row_turn_into_array(row):
     cleaned_row = row.rstrip('\n')
     array_row = cleaned_row.split(',')
@@ -8,14 +5,15 @@ def clean_row_turn_into_array(row):
 
 
 def load_in_ballot_data(csv_of_ballots_path):
-    ballots = []
-    # US7.1 read in CSV of ballots US7.3 add 1000 to start
+    ballot_list = []
     ballot_csv = open(csv_of_ballots_path, 'r')
+
     for row in ballot_csv:
         a_ballot = clean_row_turn_into_array(row)
         a_ballot.insert(0, 1000)
-        ballots.append(a_ballot)
-    return ballots
+        ballot_list.append(a_ballot)
+        
+    return ballot_list
 
 
 def load_in_number_of_vacancies():
