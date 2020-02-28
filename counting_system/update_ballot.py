6f52bd1ae2ab1e82c51c2fefe10085bb64aac193
Unknown <maxscaddie@gmail.com>
2020-02-28 12:20:53 +1100

    Some small changes

--- counting_system/update_ballot.py
+++ counting_system/update_ballot.py
@@ -1,26 +1,28 @@
-# US12 - remove pointless canidates
-def remove_pointless_candidates(ballot_list, candidate_points):
-    candiates_with_points = [cand for (pts,cand) in candidate_points]
+from calculator import ballot_get_highest_preference, ballot_get_points
+
+
+def remove_point_less_candidates(ballot_list, candidate_points):
+    candiates_with_points = [candidate for (
+        points, candidate) in candidate_points]
 
     for ballot in ballot_list:
-        index = 1
-        while index < len(ballot)):
-            current_candidate = ballot[index]
+        index_of_candidate_currently_considered = 1
+        while index_of_candidate_currently_considered < len(ballot):
+            current_candidate = ballot[index_of_candidate_currently_considered]
             if current_candidate not in candiates_with_points:
                 ballot.remove(current_candidate)
             else:
-                index += 1
+                index_of_candidate_currently_considered += 1
 
-    return ballot_list        
+    return ballot_list
 
-# US15 - apply transfer factor then remove elected candidate
-def apply_TF_remove_elected_candidate(candidate_name, ballot_list, transfer_factor):
+
+def apply_transfer_factor_remove_elected_candidate(candidate_name, ballot_list, transfer_factor):
 
     for ballot in ballot_list:
-        if ballot[1] == candidate_name:
-            ballot[0] = ballot[0] * transfer_factor
+        if ballot_get_highest_preference(ballot) == candidate_name:
+            ballot[0] = ballot_get_points(ballot) * transfer_factor
         if candidate_name in ballot:
             ballot.remove(candidate_name)
-        
+
     return ballot_list
-    
